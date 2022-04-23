68267de563fa63ca16d12d4ebeb70c21787b3a16
JohnVaiosDimopoulos <johnvaiosdim@yahoo.com>
2022-04-23 19:37:21 +0300

    Shared component test in parent-app

--- parent-app/src/App.js
+++ parent-app/src/App.js
@@ -1,48 +1,8 @@
-import logo from './logo.svg';
-import {useState} from "react";
 import './App.css';
-import {Requirements} from "@johnvaiosdimopoulos/software-engineering-project-spring-2022-team1";
+import {TestComponent} from "@johnvaiosdimopoulos/software-engineering-project-spring-2022-team1";
 
 function App() {
-    const [valid,setValid] = useState(false);
-    const [password,setPassword] = useState('');
-    const [username,setUsername] = useState('');
-
-    const passwordRequirements = [
-        {
-            text: 'Must be at least 8 characters',
-            validator: val => val.length >= 8,
-        },
-        {
-            text: 'Must contain at least one number',
-            validator: val => /\d/g.test(val),
-        },
-        {
-            text: 'Must contain at least one lower-case letter',
-            validator: val => /[a-z]/g.test(val),
-        },
-        {
-            text: 'Must contain at least one upper-case letter',
-            validator: val => /[A-Z]/g.test(val),
-        }
-    ];
-    return (
-        <div>
-            <h1>Signup</h1>
-
-            <Requirements
-                value={password}
-                requirements={passwordRequirements}
-                onValidChange={isValid => setValid(isValid)}
-            />
-
-            <input placeholder='Username' value={username} onChange={e => setUsername(e.target.value)} />
-            <input placeholder='Password' type='password' value={password} onChange={e => setPassword(e.target.value)} />
-
-            <button disabled={!valid || !username}>Sign Up</button>
-        </div>
-    );
-
+    return <TestComponent/>;
 }
 
 export default App;
