a57408adb4753425fde459b9889618742d8546e1
pspanoudakis <pavlos.spanoudakis@gmail.com>
2022-05-22 02:19:20 +0300

    Context bug fix

--- provider-app/src/App.jsx
+++ provider-app/src/App.jsx
@@ -16,29 +16,29 @@ function App() {
 
   return (
     <>
-      <AdminModal
-        show={appContext.showModal}
-        closeCallback={() => setAppContext({
-          ...appContext,
-          showModal: false,
-          modalContent: null
-      })}>
-        { appContext.modalContent}
-      </AdminModal>
-      <div className='w-full, flex justify-center h-max'>
-        <div
-          className='w-full h-max flex flex-col items-center'
-          style={{ maxWidth: "60rem", minWidth: "20rem"}}
-        >
-          <AppContext.Provider value={{
-            state: appContext,
-            setState: setAppContext
-          }}>
-            <Navbar/>
-            <Outlet/>
-          </AppContext.Provider>
+      <AppContext.Provider value={{
+        state: appContext,
+        setState: setAppContext
+      }}>
+        <AdminModal
+          show={appContext.showModal}
+          closeCallback={() => setAppContext({
+            ...appContext,
+            showModal: false,
+            modalContent: null
+        })}>
+          { appContext.modalContent }
+        </AdminModal>
+        <div className='w-full, flex justify-center h-max'>
+          <div
+            className='w-full h-max flex flex-col gap-2 items-center'
+            style={{ maxWidth: "60rem", minWidth: "20rem"}}
+          >
+              <Navbar/>
+              <Outlet/>
+          </div>
         </div>
-      </div>
+      </AppContext.Provider>
     </>
   );
 }
