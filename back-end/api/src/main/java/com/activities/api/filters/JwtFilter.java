94ca370cb2a877949485719c06b301840559a518
Loukas Mastoropoulos <sdi1700078@di.uoa.gr>
2022-05-31 18:00:01 +0300

    removed dead code

--- back-end/api/src/main/java/com/activities/api/filters/JwtFilter.java
+++ back-end/api/src/main/java/com/activities/api/filters/JwtFilter.java
@@ -64,6 +64,4 @@ public class JwtFilter extends OncePerRequestFilter {
         chain.doFilter(request, response);
     }
 
-
-
 }
--- back-end/api/src/main/java/com/activities/api/services/UserDetailsServiceImpl.java
+++ back-end/api/src/main/java/com/activities/api/services/UserDetailsServiceImpl.java
@@ -2,7 +2,6 @@ package com.activities.api.services;
 
 import com.activities.api.entities.User;
 import com.activities.api.repositories.UserRepository;
-import com.activities.api.utils.CustomPasswordEncoder;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,14 +12,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService{
 
-    @Autowired private CustomPasswordEncoder passwordEncoder;
     @Autowired private UserRepository userRepo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepo.findByUsername(username);
-        // String pass = user.getPassword();
-        // user.setPassword(passwordEncoder.getPasswordEncoder().encode(pass));
         return user;
     }
     
