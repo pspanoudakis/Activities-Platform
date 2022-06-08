64c4f4a76230d3ce46bdf3345c852a750facdd49
Loukas Mastoropoulos <sdi1700078@di.uoa.gr>
2022-06-08 21:51:00 +0300

    removed unused imports

--- back-end/api/src/main/java/com/activities/api/entities/BankCard.java
+++ back-end/api/src/main/java/com/activities/api/entities/BankCard.java
@@ -1,8 +1,6 @@
 package com.activities.api.entities;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
