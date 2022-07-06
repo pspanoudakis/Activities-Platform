30216130df21505ccdcbb2f82d94d6b15e33dc41
JohnVaiosDimopoulos <johnvaiosdim@yahoo.com>
2022-07-07 00:40:14 +0300

    Get activity Reviews for seller added

--- back-end/api/src/main/java/com/activities/api/entities/BankAccount.java
+++ back-end/api/src/main/java/com/activities/api/entities/BankAccount.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 
 @Entity @Table(name = "bank_accounts")
 @Data @AllArgsConstructor @NoArgsConstructor
-public class BanckAccount {
+public class BankAccount {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
