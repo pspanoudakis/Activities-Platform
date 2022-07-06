f85f48cf9910e5688363b25175026384ba3414cc
Loukas Mastoropoulos <sdi1700078@di.uoa.gr>
2022-07-06 07:43:57 +0300

    login and quick_login moved to user service class

--- back-end/api/src/main/java/com/activities/api/services/UserService.java
+++ back-end/api/src/main/java/com/activities/api/services/UserService.java
@@ -2,10 +2,19 @@ package com.activities.api.services;
 
 import java.util.List;
 
+import com.activities.api.dto.AuthCredentialsRequest;
+import com.activities.api.entities.Authority;
 import com.activities.api.entities.User;
 import com.activities.api.repositories.UserRepository;
+import com.activities.api.utils.JwtUtil;
 
+import org.hibernate.procedure.ParameterMisuseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Pair;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 
@@ -13,6 +22,50 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     @Autowired private UserRepository userRepo;
+    @Autowired private AuthenticationManager authenticationManager;
+    @Autowired private AuthorityService authorityService;
+    @Autowired private JwtUtil jwtUtil;
+
+    public Pair<String, User> login(AuthCredentialsRequest request, String role) throws BadCredentialsException, ParameterMisuseException{
+        Authentication authenticate = authenticationManager
+            .authenticate(
+                new UsernamePasswordAuthenticationToken(
+                    request.getUsername(), request.getPassword()
+                )
+            );
+
+        User user = (User) authenticate.getPrincipal();
+        Authority user_role = authorityService.getAuthority(role);
+
+        if(user_role == null)
+            throw new ParameterMisuseException("given role " + role + " not valid");
+        if(!user.getAuthorities().contains(user_role))
+            throw new BadCredentialsException("user is not of given role " + role);
+
+        String token = jwtUtil.generateToken(user);            
+        user.setPassword(null);
+        
+        return Pair.of(token, user);
+    }
+
+    public Pair<String, User> quick_login(String token, String role) throws BadCredentialsException, ParameterMisuseException{
+        
+        User user = getUserByUN(jwtUtil.getUsernameFromToken(token));
+
+        if(user == null)
+            throw new BadCredentialsException("no user with credential specified by given token");     
+        if(jwtUtil.validateToken(token, user) == false)
+            throw new BadCredentialsException("Token not valid");
+
+        Authority user_role = authorityService.getAuthority(role);
+
+        if(user_role == null)
+            throw new ParameterMisuseException("role " + role + " is not valid");
+        if(!user.getAuthorities().contains(user_role))
+            throw new BadCredentialsException("user is not of given role " + role);
+
+        return Pair.of(token, user);
+    }
 
     public List<User> getAllUsers(){
         return userRepo.findAll();
