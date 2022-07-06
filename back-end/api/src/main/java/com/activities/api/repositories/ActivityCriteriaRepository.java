dce38cfc63a543174c832f0f24829bd1bae2f60d
Loukas Mastoropoulos <sdi1700078@di.uoa.gr>
2022-07-06 18:58:20 +0300

    splited text search and the rest of the filters

--- back-end/api/src/main/java/com/activities/api/repositories/ActivityCriteriaRepository.java
+++ back-end/api/src/main/java/com/activities/api/repositories/ActivityCriteriaRepository.java
@@ -5,14 +5,12 @@ import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
