adecca854faf9a3515fc79598be908881dc0a1bb
pspanoudakis <pavlos.spanoudakis@gmail.com>
2022-07-07 13:28:38 +0300

    Fixed invalid exports

--- common-components-lib/src/components/placeholders/placeholders.js
+++ common-components-lib/src/components/placeholders/placeholders.js
@@ -1,9 +1,7 @@
-export const placeholders = {
-    PLACEHOLDER_ACTIVITY_IMG: 'https://user-images.githubusercontent.com/2351721/31314483-7611c488-ac0e-11e7-97d1-3cfc1c79610e.png',
-    PLACEHOLDER_USER_IMG: 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541',
-    
-    defaultHomePosition: {
-        "lat": 38.11987459663194,
-        "lng": 23.866071050478975
-    }
+export const PLACEHOLDER_ACTIVITY_IMG = 'https://user-images.githubusercontent.com/2351721/31314483-7611c488-ac0e-11e7-97d1-3cfc1c79610e.png'
+export const PLACEHOLDER_USER_IMG = 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541'
+
+export const defaultHomePosition = {
+    "lat": 38.11987459663194,
+    "lng": 23.866071050478975
 }
